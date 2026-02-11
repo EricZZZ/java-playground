b97b1713ebf6a91cad785057922f08f7d07a1731
EricZzz <biggogten@gmail.com>
2026-02-12 01:27:02 +0800

    feat: 添加接口A和B，以及实现类C，支持多态示例；添加Shop类以计算产品价格

---
+++ src/com/ericzzz/j8/A.java
@@ -0,0 +1,7 @@
+package com.ericzzz.j8;
+
+public interface A {
+    default void hello() {
+        System.out.println("Hello from A");
+    }
+}
---
+++ src/com/ericzzz/j8/C.java
@@ -0,0 +1,7 @@
+package com.ericzzz.j8;
+
+public class C implements B, A {
+    public static void main(String[] args) {
+        new C().hello();
+    }
+}
